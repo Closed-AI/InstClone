aeeb1eebcc3e4310ba1d35acd40696ffbc9e2b69
ARS <closedAI@mail.ru>
2022-11-08 11:14:14 +0300

    add creator id in post model

--- API/Models/PostModel.cs
+++ API/Models/PostModel.cs
@@ -3,6 +3,7 @@
     public class PostModel
     {
         public Guid Id { get; set; }
+        public Guid CreatorId { get; set; }
         public string? Description { get; set; }
         public DateTimeOffset CreatingDate { get; set; }
         public List<string>? ContentLinks { get; set; }
--- DAL/Entities/Post.cs
+++ DAL/Entities/Post.cs
@@ -3,9 +3,9 @@
     public class Post
     {
         public Guid Id { get; set; }
+        public Guid CreatorId { get; set; }
         public string? Description { get; set; }
         public DateTimeOffset CreatingDate { get; set; }
-        public Guid CreatorId { get; set; }
 
         public virtual User Creator { get; set; } = null!;
         public virtual ICollection<Comment>? Comments { get; set; }
